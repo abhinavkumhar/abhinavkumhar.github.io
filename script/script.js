4f08730eafe9c8862f7426540b724b9c6cda30b9
Abhinav Kumhar <abhinavkumhar21@gmail.com>
2023-12-22 15:34:28 +0530

    Fix the navbar open default problem

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 let nav_button = document.querySelector('.hamburger');
 let nav_header = document.querySelector('.header-class');
+let nav_list_button = document.querySelector('.nav-btns');
 const toggleNavbar = ()=>{
-    // alert("hi doraemon")
+    nav_header.classList.toggle('active');
+}
+const toggleNavList = ()=>{
     nav_header.classList.toggle('active');
 }
 nav_button.addEventListener('click', ()=> toggleNavbar());
+nav_list_button.addEventListener('click', ()=> toggleNavList());
